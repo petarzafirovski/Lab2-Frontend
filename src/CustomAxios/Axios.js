a19610ca254c0f5a0d4a3107521e1c6f5123c9e5
Petar <zafirovskipetar@gmail.com>
2022-04-08 17:36:02 +0200

    added headers

--- src/CustomAxios/Axios.js
+++ src/CustomAxios/Axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL:"https://elibrary-backend-191537.herokuapp.com/api"
+    baseURL:"https://elibrary-backend-191537.herokuapp.com/api",
+    headers: {
+        'Access-Control-Allow-Origin': '*'
+    }
 })
 
 export default instance;
